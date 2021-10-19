cb7733ecba0a563586222f1257fdced3180eb032
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2021-10-19 14:51:48 +0530

    Maximum Rectangular Area Of A Histogram

---
+++ MaximumRectangularAreaOfAhistogram.java
@@ -0,0 +1,42 @@
+package GitHub;
+
+public class MaximumRectangularAreaOfAhistogram {
+	class Solution
+	{
+	    //Function to find largest rectangular area possible in a given histogram.
+	   public static long getMaxArea(long [] hist, long n)
+	    {
+	        // your code here
+	        int [] left  = new int[(int)n];
+	        int [] right = new int[(int)n];
+	        Stack<Integer> st = new Stack<>();
+
+	        for(int i=0;i<n;i++){
+	            while(!st.empty() && hist[i] <= hist[st.peek()]){
+	                st.pop();
+	            }
+
+	            if(st.empty()) left[i]=0;
+	            else left[i]=st.peek()+1;
+	            st.push(i);
+	        }
+	        st=new Stack<>();
+	        for(int i=(int)n-1;i>=0;i--){
+	            while(!st.empty() && hist[i] <= hist[st.peek()]){
+	                st.pop();
+	            }
+	            if(st.empty()) right[i]=(int)n-1;
+	            else right[i]=st.peek()-1;
+	            st.push(i);
+	        }
+	        long max=0;
+	        for(int i=0;i<n;i++){
+	            long i1 = right[i] - left[i] + 1;
+	            if(max < i1 * hist[i])
+	                max= i1 * hist[i];
+	        }
+	        return max;
+	    }
+	        
+	}
+}
