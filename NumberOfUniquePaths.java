c4648cbca650ac18fda3f9fbac335b2090b9ccee
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2021-10-28 20:31:33 +0530

    Number of Unique Paths

---
+++ NumberOfUniquePaths.java
@@ -0,0 +1,21 @@
+
+public class NumberOfUniquePaths {
+	class Solution
+	{
+	    //Function to find total number of unique paths.
+	    public static int NumberOfPath(int a, int b)
+	    {
+	        int[][] matrix = new int[a][b];
+	        for(int i=0;i<a;i++) {
+	            for(int j=0;j<b;j++) {
+	                if(i==0||j==0) { // i-only from left, j-only from top
+	                    matrix[i][j]=1;
+	                }else {
+	                    matrix[i][j] = matrix[i][j-1]+matrix[i-1][j];//left+top
+	                }
+	            }
+	        }
+	        return matrix[a-1][b-1];
+	    }
+	}
+}
