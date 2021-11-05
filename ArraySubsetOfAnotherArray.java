1752ece41608f92a3b0c537f27c7d9a18cbd99fc
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2021-11-05 21:06:43 +0530

    ArraySubsetOfAnotherArray

---
+++ ArraySubsetOfAnotherArray.java
@@ -0,0 +1,27 @@
+
+public class ArraySubsetOfAnotherArray {
+	class Compute {
+	    public String isSubset( long a1[], long a2[], long n, long m) {
+
+	        HashMap<Long,Integer> freq = new HashMap<>();
+	        for(int i=0;i<n;i++){
+	            if(!freq.containsKey(a1[i])) freq.put(a1[i],1);
+	            else {
+	                int count = freq.get(a1[i]);
+	                freq.put(a1[i],count+1);
+	            }
+	        }
+	        for(int i=0;i<m;i++){
+	            if(!freq.containsKey(a2[i])) return "No";
+	            else {
+	                int count = freq.get(a2[i]);
+	                count--;
+	                if(count<0) return "No";
+	                else freq.put(a2[i],count);
+	            }
+	        }
+	        return "Yes";
+
+	    }
+	}
+}
