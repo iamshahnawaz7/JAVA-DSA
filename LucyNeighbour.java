81ceb7346a04f3711e5a33c278951d79e6335b89
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2021-11-17 23:51:08 +0530

    Lucy's Neighbour

---
+++ LucyNeighbour.java
@@ -0,0 +1,43 @@
+
+public class LucyNeighbour {
+	class Solution 
+	{ 
+		public static ArrayList<Integer> Kclosest(int arr[], int n, int x, int k)
+	    {
+	        // Your code goes here
+	        ArrayList<Integer> neigh = new ArrayList<>();
+	        Arrays.sort(arr);
+	        int idx = 0;
+	        while(idx < n && arr[idx]<x){
+	            idx++;
+	        }
+	        idx--;
+	        int low = idx,high=idx+1,count=0;
+	        while(count<k && (low>=0 || high<n)){
+	            int d1 = low>=0 ? Math.abs(arr[low]-x) : Integer.MAX_VALUE;
+	            int d2 = high<n ? Math.abs(arr[high]-x) : Integer.MAX_VALUE;
+	            if(d1<d2) {
+	                neigh.add(arr[low]);
+	                low--;
+	            }
+	            else if(d2<d1){
+	                neigh.add(arr[high]);
+	                high++;
+	            }
+	            else {
+	                if(arr[low]<arr[high]) {
+	                    neigh.add(arr[low]);
+	                    low--;
+	                }
+	                else {
+	                    neigh.add(arr[high]);
+	                    high++;
+	                }
+	            }
+	            count++;
+	        }
+	        Collections.sort(neigh);
+	        return neigh;
+	    }
+	}
+}
