2e3282c91b58bfc3678257ce2e36bd2dab7479e0
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2021-11-27 22:33:35 +0530

    Add files via upload

---
+++ FindNthRootOfM.java
@@ -0,0 +1,22 @@
+
+public class FindNthRootOfM {
+	class Solution
+	{
+	    public int NthRoot(int n, int m)
+	        {
+	        int low=1,high=m;
+
+	               while(low<=high){
+	                   int mid=(high+low)/2;
+	                   int nthpow=(int)Math.pow(mid,n);
+	                   if(nthpow==m)
+	                       return mid;
+	                   else if(nthpow>m)
+	                       high=mid-1;
+	                   else
+	                       low=mid+1;
+	               }
+	               return -1;
+	        }
+	}
+}
