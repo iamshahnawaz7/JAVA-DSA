aa0e4d33d1f589d9afbd077358e17241c5ec181a
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2021-12-13 20:39:47 +0530

    Find n/k th node in Linked list

---
+++ FindNByKthNodeAndReturn.java
@@ -0,0 +1,28 @@
+package Quesba;
+
+public class FindNByKthNodeAndReturn {
+	class GfG
+	{
+	    public static int getSize(Node head){
+	        int count=0;
+	        while(head!=null){
+	            count++;
+	            head = head.next;
+	        }
+	        return count;
+	    }
+	    public static int nknode(Node head, int k)
+	    {
+	        // add your code here
+	        int n = getSize(head);
+	        int c = (int) n/k;
+	        if(n%k!=0) c++;
+	        Node temp = head;
+	        while(c!=1 && temp!=null){
+	            temp = temp.next;
+	            c--;
+	        }
+	        return temp==null ? -1 : temp.data;
+	    }
+	}
+}
