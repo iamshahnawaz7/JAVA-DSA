ef19797299f6edbf593396cb28098312b2dfda80
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2021-12-19 23:36:07 +0530

    EncryptTheString

---
+++ EncryptTheString.java
@@ -0,0 +1,28 @@
+package Quesba;
+
+public class EncryptTheString {
+	class Solution {
+	    static String encryptString(String S){
+	        // code here
+	        int i=0,j=0;
+	        ArrayList<Character> chars = new ArrayList<>();
+	        ArrayList<Integer> freq = new ArrayList<>();
+	        while(j<S.length()){
+	            int curr = 0;
+	            while(j<S.length() && S.charAt(i)==S.charAt(j)){
+	                curr++;
+	                j++;
+	            }
+	            chars.add(S.charAt(i));
+	            freq.add(curr);
+	            i = j;
+	        }
+	        String res="";
+	        for(i=freq.size()-1;i>=0;i--){
+	            String hex = Integer.toHexString(freq.get(i));
+	            res += hex+chars.get(i);
+	        }
+	        return res;
+	    }
+	};
+}
