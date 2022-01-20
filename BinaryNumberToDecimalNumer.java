6189cbd4394e3b0f4af3c07d698edd8869e3c659
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2022-01-20 10:59:00 +0530

    Binary Number To Decimal Number

---
+++ BinaryNumberToDecimalNumer.java
@@ -0,0 +1,11 @@
+package Quesba;
+
+public class BinaryNumberToDecimalNumer {
+	class Solution
+	{
+	   public int binary_to_decimal(String str)
+	   {
+	       return Integer.parseInt(str, 2);
+	   }
+	}
+}
