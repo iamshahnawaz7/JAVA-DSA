04df5fd014bc82f5bd07a29c7f4a4036313661d6
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2022-03-07 19:13:08 +0530

    Merge K Sorted List

---
+++ MergeKSortedLinkedList.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class MergeKSortedLinkedList {
+	class Solution
+    {
+        Node mergeKList(Node[]arr, int K)
+        {
+            Node merged = new Node(-1);
+            merged.next = arr[0];
+            for(int i=1;i<K;i++){
+                Node curr = arr[i];
+                Node temp = merged;
+                while(curr!=null && temp!=null){
+                    while(temp.next!=null && temp.next.data<=curr.data){
+                        temp = temp.next;
+                    }
+                    Node next = temp.next;
+                    temp.next = curr;
+                    curr = curr.next;
+                    temp= temp.next;
+                    temp.next = next;
+                }
+            }
+            return merged.next;
+        }
+}
+}
