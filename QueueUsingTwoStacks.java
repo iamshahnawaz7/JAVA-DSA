ca6a2db409a728b295e104f6a8715d97b53b85d8
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2022-03-09 10:46:34 +0530

    Queue Using Two Stacks

---
+++ QueueUsingTwoStacks.java
@@ -0,0 +1,35 @@
+package Recursion;
+
+public class QueueUsingTwoStacks {
+	class StackQueue
+	{
+	    Stack<Integer> s1 = new Stack<Integer>();
+	    Stack<Integer> s2 = new Stack<Integer>();
+	    //Queue<Integer> q1 = new LinkedList<Integer>();
+	    //Function to push an element in queue by using 2 stacks.
+	    void Push(int x)
+	    {
+		  s1.push(x);
+
+	    }
+		
+	    
+	    //Function to pop an element from queue by using 2 stacks.
+	    int Pop()
+	    {
+		   if(s1.isEmpty()) {
+		       return -1;
+		   }
+		   
+		   while(!s1.isEmpty()){
+		      s2.push(s1.pop());
+		      }
+		      int a=s2.pop();
+		     while(!s2.isEmpty()){
+		         s1.push(s2.pop());
+		     } 
+		     return a;
+	    }
+	}
+
+}
