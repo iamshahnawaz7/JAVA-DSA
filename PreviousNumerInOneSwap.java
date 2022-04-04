179ee3ae9d48cabcb26056a96a79f241243ec689
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2022-04-04 20:08:42 +0530

    Previous number in one swap

---
+++ PreviousNumerInOneSwap.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class PreviousNumerInOneSwap {
+	class Solution{
+	    static String previousNumber(String s){
+	       int n=s.length();
+	       int largest=n-2;
+	       while(largest>=0&&(s.charAt(largest)-'0')<=(s.charAt(largest+1)-'0')) largest--;
+	       if(largest<0) return "-1";
+	       int smallest=n-1;
+	       while(smallest>largest&&(s.charAt(smallest)-'0')>=(s.charAt(largest)-'0')) smallest--;
+	       while(smallest>0&&(s.charAt(smallest)-'0')==(s.charAt(smallest-1)-'0')) smallest--;
+	       List<Character> ls=new LinkedList<>();
+	       for(int i=0;i<n;i++) ls.add(s.charAt(i));
+	       Collections.swap(ls,largest,smallest);
+	       String st=ls
+	       .toString()
+	       .replace(", ","")
+	       .replace("]","")
+	       .replace("[","");
+	       if((st.charAt(0)-'0')==0) return "-1";
+	       return st;
+	    }
+	}
+
+}
