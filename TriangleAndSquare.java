d9f98c404d69b38a2db3895889ec29df47b353ec
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2022-04-05 09:57:08 +0530

    Triangle And Square

---
+++ TriangleAndSquare.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+public class TriangleAndSquare {
+	class Solution{
+		int countSquare(int B)
+		   {
+		    int num = B/2 - 1;   
+		    return num * (num + 1)/2; 
+		   }
+		}
+}
