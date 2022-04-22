29f1d4eaa03ae4d1068b7d8276c49865cee3b94f
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2022-04-22 23:07:27 +0530

    Sum of two numbers without using arithmetic operators

---
+++ SumOfTwoNumbersWithoutUsingArithematicOperators.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class SumOfTwoNumbersWithoutUsingArithematicOperators {
+	class Solution
+	{
+	   //count=0;
+	   int res;
+	   int sum(int a , int b)
+	   {
+	       //code here
+	       for(int i=1;i<=b;i++){
+	       a++;
+	       res=a;
+	     //  System.out.println(res);
+	       }
+	     //  System.out.pres)res);
+	     return res;
+	   }
+}
