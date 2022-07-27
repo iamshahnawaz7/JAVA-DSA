eb57663744d7eb106c0eaff4b3d4b16d303fb40d
Shahnawaz Alam <77240648+iamshahnawaz7@users.noreply.github.com>
2022-07-27 12:53:03 +0530

    Merge Sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,7 +1,53 @@
 package Sorting;
-
+import java.util.*;
 public class MergeSort {
-    public static void main(String[] args) {
+public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of elements you want to insert in the array");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter the elements now");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+        arr=sort(arr);
+        System.out.println("The sorted array is "+Arrays.toString(arr));
+	}
 
+public static int[] sort(int [] nums){
+    if(nums.length<=1){
+        return nums;
     }
+    int mid = nums.length/2;
+    int [] left = Arrays.copyOfRange(nums,0,mid);
+    int [] right = Arrays.copyOfRange(nums,mid,nums.length);
+    return merge(sort(left),sort(right));
+}
+
+public static int[] merge(int[]a,int[]b) {
+	int i=0,k=0,j=0;
+	int[] merged=new int[a.length+b.length];
+	while(i<a.length && j<b.length) {
+		if(a[i]<b[j]) {
+			merged[k]=a[i];
+			i++;
+		}
+		else {
+			merged[k]=b[j];
+			j++;
+		}
+		k++;
+	}
+	while(i<a.length) {
+		merged[k]=a[i];
+		i++;
+		k++;
+	}
+	while(j<b.length) {
+		merged[k]=b[j];
+		j++;
+		k++;
+	}
+	return merged;
+}
 }
